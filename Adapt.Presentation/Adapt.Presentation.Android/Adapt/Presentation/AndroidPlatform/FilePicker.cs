11c564a1cfb88e1fe48dae274783b2cbbddcd6d2
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-05-04 10:30:13 +1000

    Add the Android specific File Picker code.

---
+++ Adapt.Presentation/Adapt.Presentation.Android/Adapt/Presentation/AndroidPlatform/FilePicker.cs
@@ -0,0 +1,131 @@
+using Adapt.Presentation;
+using Android.Content;
+using Android.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Adapt.Presentation.AndroidPlatform
+{
+    /// <summary>
+    /// Implementation for Feature
+    /// </summary>
+    ///
+    [Preserve(AllMembers = true)]
+    public class FilePicker : IFilePicker
+    {
+        #region Fields
+        private int _requestId;
+        private TaskCompletionSource<FileData> _completionSource;
+        private Context _Context;
+        #endregion
+
+        #region Constructor
+        public FilePicker(Context context)
+        {
+            _Context = context;
+        }
+        #endregion
+
+        #region Private Methods
+        private int GetRequestId()
+        {
+            int id = _requestId;
+
+            if (_requestId == int.MaxValue)
+                _requestId = 0;
+            else
+                _requestId++;
+
+            return id;
+        }
+
+        private Task<FileData> PickAndOpenFile(bool isSave)
+        {
+            var id = GetRequestId();
+
+            var ntcs = new TaskCompletionSource<FileData>(id);
+
+            if (Interlocked.CompareExchange(ref _completionSource, ntcs, null) != null)
+                throw new InvalidOperationException("Only one operation can be active at a time");
+
+            try
+            {
+                var pickerIntent = new Intent(_Context, typeof(FilePickerActivity));
+                pickerIntent.SetFlags(ActivityFlags.NewTask);
+
+                _Context.StartActivity(pickerIntent);
+
+                EventHandler<FilePickerEventArgs> handler = null;
+                EventHandler<EventArgs> cancelledHandler = null;
+
+                handler = (s, e) =>
+                {
+                    var tcs = Interlocked.Exchange(ref _completionSource, null);
+
+                    FilePickerActivity.FilePicked -= handler;
+
+                    Stream fileStream;
+                    if (isSave)
+                    {
+                        fileStream = File.OpenRead(e.FilePath);
+                    }
+                    else
+                    {
+                        fileStream = File.OpenWrite(e.FilePath);
+                    }
+
+                    tcs?.SetResult(new FileData { FileName = e.FileName, FileStream = fileStream });
+                };
+
+                cancelledHandler = (s, e) =>
+                {
+                    var tcs = Interlocked.Exchange(ref _completionSource, null);
+
+                    FilePickerActivity.FilePickCancelled -= cancelledHandler;
+
+                    tcs?.SetResult(null);
+                };
+
+                FilePickerActivity.FilePickCancelled += cancelledHandler;
+                FilePickerActivity.FilePicked += handler;
+            }
+            catch (Exception exAct)
+            {
+                Debug.Write(exAct);
+            }
+
+            return _completionSource.Task;
+        }
+
+        #endregion
+
+        #region Public Methods (Implementation)
+        public Task<FileData> PickAndOpenFileForReading()
+        {
+            return PickAndOpenFile(false);
+        }
+
+        public async Task<FileData> PickAndOpenFileForWriting(IDictionary<string, IList<string>> fileTypes, string fileName)
+        {
+            var directory = Path.Combine(Android.OS.Environment.ExternalStorageDirectory.AbsolutePath, Android.OS.Environment.DirectoryDownloads);
+
+            var filePath = Path.Combine(directory, fileName);
+
+            var retVal = new FileData { FileName = fileName };
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            retVal.FileStream = File.Create(filePath);
+
+            return retVal;
+        }
+        #endregion
+    }
+}
---
+++ Adapt.Presentation/Adapt.Presentation.Android/Adapt/Presentation/AndroidPlatform/FilePickerActivity.cs
@@ -0,0 +1,120 @@
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Provider;
+using Android.Runtime;
+using System;
+
+namespace Adapt.Presentation.AndroidPlatform
+{
+    [Activity(ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize)]
+    [Preserve(AllMembers = true)]
+    public class FilePickerActivity : Activity
+    {
+        #region Protected Overrides
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            var intent = new Intent(Intent.ActionGetContent);
+            intent.SetType("*/*");
+
+            intent.AddCategory(Intent.CategoryOpenable);
+            try
+            {
+                StartActivityForResult(Intent.CreateChooser(intent, "Select file"), 0);
+            }
+            catch (Exception exAct)
+            {
+                System.Diagnostics.Debug.Write(exAct);
+            }
+        }
+
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            if (resultCode == Result.Canceled)
+            {
+                // Notify user file picking was cancelled.
+                OnFilePickCancelled();
+                Finish();
+            }
+            else
+            {
+                System.Diagnostics.Debug.Write(data.Data);
+                try
+                {
+                    var _uri = data.Data;
+
+                    var filePath = IOUtil.getPath(ApplicationContext, _uri);
+
+                    if (string.IsNullOrEmpty(filePath))
+                        filePath = _uri.Path;
+
+                    var file = IOUtil.readFile(filePath);
+
+                    var fileName = GetFileName(ApplicationContext, _uri);
+
+                    OnFilePicked(new FilePickerEventArgs(file, fileName, filePath));
+                }
+                catch (Exception readEx)
+                {
+                    // Notify user file picking failed.
+                    OnFilePickCancelled();
+                    System.Diagnostics.Debug.Write(readEx);
+                }
+                finally
+                {
+                    Finish();
+                }
+            }
+        }
+        #endregion
+
+        #region Internal Static Events
+        internal static event EventHandler<FilePickerEventArgs> FilePicked;
+        internal static event EventHandler<EventArgs> FilePickCancelled;
+        #endregion
+
+        #region Private Methods
+        private string GetFileName(Context ctx, Android.Net.Uri uri)
+        {
+
+            string[] projection = { MediaStore.MediaColumns.DisplayName };
+
+            var cr = ctx.ContentResolver;
+            var name = "";
+            var metaCursor = cr.Query(uri, projection, null, null, null);
+
+            if (metaCursor != null)
+            {
+                try
+                {
+                    if (metaCursor.MoveToFirst())
+                    {
+                        name = metaCursor.GetString(0);
+                    }
+                }
+                finally
+                {
+                    metaCursor.Close();
+                }
+            }
+            return name;
+        }
+        #endregion
+
+        #region Private Static Methods
+        private static void OnFilePickCancelled()
+        {
+            FilePickCancelled?.Invoke(null, null);
+        }
+
+        private static void OnFilePicked(FilePickerEventArgs e)
+        {
+            FilePicked?.Invoke(null, e);
+        }
+        #endregion
+    }
+}
