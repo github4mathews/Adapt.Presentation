31509c62a37d6852a17ea97558e23a8a62a49dcc
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-05-04 15:52:54 +1000

    Fix a namespace

--- Adapt.Presentation/Samples/Adapt.PresentationSamples.UWP/App.xaml.cs
+++ Adapt.Presentation/Samples/Adapt.PresentationSamples.UWP/App.xaml.cs
@@ -5,12 +5,12 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-namespace Adapt.Presentation.UWP
+namespace Adapt.PresentationSamples.UWP
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    public sealed partial class App : Application
     {
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
