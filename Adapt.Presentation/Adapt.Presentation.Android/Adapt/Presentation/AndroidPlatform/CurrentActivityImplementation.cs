fefae63fe74369e3d7ab04867796df05564023b6
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-05-04 12:24:53 +1000

    Add the media plugin and permissions abstractions as well as the Android implementation.

---
+++ Adapt.Presentation/Adapt.Presentation.Android/Adapt/Presentation/AndroidPlatform/CurrentActivityImplementation.cs
@@ -0,0 +1,22 @@
+using Android.App;
+using System;
+
+
+namespace Adapt.Presentation.AndroidPlatform
+{
+    /// <summary>
+    /// Implementation for Feature
+    /// </summary>
+    public class CurrentActivityImplementation : ICurrentActivity
+    {
+        /// <summary>
+        /// Gets or sets the activity.
+        /// </summary>
+        /// <value>The activity.</value>
+        public Activity Activity
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Adapt.Presentation/Adapt.Presentation.Standard/Adapt/Presentation/CrossPermissions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Adapt.Presentation
+{
+    /// <summary>
+    /// Cross platform Permissions implemenations
+    /// </summary>
+    public static class CrossPermissions
+    {
+        static Lazy<IPermissions> Implementation = new Lazy<IPermissions>(CreatePermissions, System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        /// <summary>
+        /// Current settings to use
+        /// </summary>
+        public static IPermissions Current
+        {
+            get
+            {
+                var ret = Implementation.Value;
+                if (ret == null)
+                {
+                    throw NotImplementedInReferenceAssembly();
+                }
+                return ret;
+            }
+        }
+
+        static IPermissions CreatePermissions()
+        {
+            return null;
+        }
+
+        internal static Exception NotImplementedInReferenceAssembly()
+        {
+            return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+        }
+    }
+}
