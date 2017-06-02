f839ee9ba9a0dc53412428f34e22de2a67323a33
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-02 13:29:22 +1000

    First stab at DateTimePicker from here https://forums.xamarin.com/discussion/comment/137060/#Comment_137060

---
+++ Adapt.Presentation.Standard/Adapt/Presentation/Controls/DateTimePicker.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Adapt.Presentation.Controls
+{
+    public class DateTimePicker : StackLayout
+    {
+        #region Fields
+        private readonly DatePicker _Date;
+        private readonly TimePicker _Time;
+        #endregion
+
+        #region Bindable Properties
+        public static readonly BindableProperty ValueProperty =
+        BindableProperty.Create<DateTimePicker, DateTime>
+        (
+            p => p.Value,
+            defaultValue: default(DateTime),
+            defaultBindingMode: BindingMode.OneWay,
+            propertyChanging: (bindable, oldValue, newValue) =>
+            {
+                ValueChanging(bindable, oldValue, newValue);
+            }
+        );
+
+        private static void ValueChanging(BindableObject bindable, DateTime oldValue, DateTime newValue)
+        {
+            var ctrl = (DateTimePicker)bindable;
+            ctrl._Date.Date = newValue;
+        }
+        #endregion
+
+        #region Public Properties
+        public DateTime Value
+        {
+            get => (DateTime)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+        #endregion
+
+        #region Constructor
+        public DateTimePicker()
+        {
+            Orientation = StackOrientation.Vertical;
+            Padding = 2;
+            _Date = new DatePicker { Format = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern };
+            _Time = new TimePicker { Format = CultureInfo.CurrentCulture.DateTimeFormat.ShortTimePattern };
+            Children.Add(_Date);
+            Children.Add(_Time);
+            _Date.PropertyChanged += DateOnPropertyChanged;
+            _Time.PropertyChanged += TimeOnPropertyChanged;
+            _Date.Date = new DateTime();
+            _Time.Time = new DateTime().TimeOfDay;
+        }
+        #endregion
+
+        #region Event Handlers
+        private void TimeOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (propertyChangedEventArgs.PropertyName == "Time")
+            {
+                Value = _Date.Date.Add(_Time.Time);
+            }
+        }
+        private void DateOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        {
+            if (propertyChangedEventArgs.PropertyName == "Date")
+            {
+                Value = _Date.Date.Add(_Time.Time);
+            }
+        }
+        #endregion
+    }
+}
