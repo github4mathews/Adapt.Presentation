37768c25e2f4023bc897eab871adb762d9839638
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-02 14:39:45 +1000

    Add Clear and Now buttons as well as a Dispose method.

--- Adapt.Presentation.Standard/Adapt/Presentation/Controls/DateTimePicker.cs
+++ Adapt.Presentation.Standard/Adapt/Presentation/Controls/DateTimePicker.cs
@@ -6,11 +6,13 @@ using Xamarin.Forms;
 
 namespace Adapt.Presentation.Controls
 {
-    public class DateTimePicker : WrapLayout
+    public class DateTimePicker : WrapLayout, IDisposable
     {
         #region Fields
         private readonly DatePicker _Date;
         private readonly TimePicker _Time;
+        private readonly Button _ClearButton;
+        private readonly Button _NowButton;
         private bool _IsChanging;
         #endregion
 
@@ -51,24 +53,35 @@ namespace Adapt.Presentation.Controls
             Padding = 2;
             _Date = new DatePicker { Format = CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern };
             _Time = new TimePicker { Format = CultureInfo.CurrentCulture.DateTimeFormat.ShortTimePattern };
+            _ClearButton = new Button { Text = "Clear" };
+            _NowButton = new Button { Text = "Now" };
+            Children.Add(_ClearButton);
+            Children.Add(_NowButton);
             Children.Add(_Date);
             Children.Add(_Time);
             _Date.PropertyChanged += PropertyChanged;
             _Time.PropertyChanged += PropertyChanged;
+            _ClearButton.Clicked += ClearButton_Clicked;
+            _NowButton.Clicked += NowButton_Clicked;
             _Date.Date = new DateTime();
             _Time.Time = new DateTime().TimeOfDay;
         }
         #endregion
 
         #region Event Handlers
+
+        private void NowButton_Clicked(object sender, EventArgs e)
+        {
+            Value = DateTime.Now;
+        }
+
+        private void ClearButton_Clicked(object sender, EventArgs e)
+        {
+            Value = DateTime.MinValue;
+        }
+
         private void PropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
         {
-            //This is just here for debugging purposes
-            if(propertyChangedEventArgs.PropertyName== "IsFocused")
-            {
-                return;
-            }
-
             if (_IsChanging)
             {
                 return;
@@ -80,6 +93,16 @@ namespace Adapt.Presentation.Controls
             }
 
             Value = _Date.Date.Add(_Time.Time);
+        }
+        #endregion
+
+        #region Public Methods
+        public void Dispose()
+        {
+            _Date.PropertyChanged -= PropertyChanged;
+            _Time.PropertyChanged -= PropertyChanged;
+            _ClearButton.Clicked -= ClearButton_Clicked;
+            _NowButton.Clicked -= NowButton_Clicked;
         }
         #endregion
     }
