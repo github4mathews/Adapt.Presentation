6b2b07310d531364619d5dbdf69d023803854a92
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-02 13:58:04 +1000

    Some fixes to the DateTimePicker

--- Adapt.Presentation.Standard/Adapt/Presentation/Controls/DateTimePicker.cs
+++ Adapt.Presentation.Standard/Adapt/Presentation/Controls/DateTimePicker.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
+using System.Linq;
 using Xamarin.Forms;
 
 namespace Adapt.Presentation.Controls
@@ -10,6 +12,7 @@ namespace Adapt.Presentation.Controls
         #region Fields
         private readonly DatePicker _Date;
         private readonly TimePicker _Time;
+        private bool _IsChanging;
         #endregion
 
         #region Bindable Properties
@@ -28,7 +31,16 @@ namespace Adapt.Presentation.Controls
         private static void ValueChanging(BindableObject bindable, DateTime oldValue, DateTime newValue)
         {
             var ctrl = (DateTimePicker)bindable;
+
+            if (ctrl._IsChanging)
+            {
+                return;
+            }
+
+            ctrl._IsChanging = true;
             ctrl._Date.Date = newValue;
+            ctrl._Time.Time = newValue.TimeOfDay;
+            ctrl._IsChanging = false;
         }
         #endregion
 
@@ -49,24 +61,17 @@ namespace Adapt.Presentation.Controls
             _Time = new TimePicker { Format = CultureInfo.CurrentCulture.DateTimeFormat.ShortTimePattern };
             Children.Add(_Date);
             Children.Add(_Time);
-            _Date.PropertyChanged += DateOnPropertyChanged;
-            _Time.PropertyChanged += TimeOnPropertyChanged;
+            _Date.PropertyChanged += PropertyChanged;
+            _Time.PropertyChanged += PropertyChanged;
             _Date.Date = new DateTime();
             _Time.Time = new DateTime().TimeOfDay;
         }
         #endregion
 
         #region Event Handlers
-        private void TimeOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
+        private void PropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
         {
-            if (propertyChangedEventArgs.PropertyName == "Time")
-            {
-                Value = _Date.Date.Add(_Time.Time);
-            }
-        }
-        private void DateOnPropertyChanged(object sender, PropertyChangedEventArgs propertyChangedEventArgs)
-        {
-            if (propertyChangedEventArgs.PropertyName == "Date")
+            if (new List<string> { nameof(TimePicker.Time), nameof(DatePicker.DateProperty) }.Contains(propertyChangedEventArgs.PropertyName))
             {
                 Value = _Date.Date.Add(_Time.Time);
             }
