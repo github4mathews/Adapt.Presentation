4fd1c5193c224486db75892f20125c763243b146
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-16 13:09:47 +1000

    Add the EntryValidationBehaviour

---
+++ Adapt.Presentation.Standard/Adapt/Presentation/Behaviours/EntryValidationBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Adapt.Presentation.Behaviours
+{
+    public static class EntryValidationBehaviour
+    {
+        public static readonly BindableProperty AttachBehaviorProperty =
+            BindableProperty.CreateAttached(
+                "AttachBehavior",
+                typeof(bool),
+                typeof(EntryValidationBehaviour),
+                false,
+                propertyChanged: OnAttachBehaviorChanged);
+
+        public static bool GetAttachBehavior(BindableObject view)
+        {
+            return (bool)view.GetValue(AttachBehaviorProperty);
+        }
+
+        public static void SetAttachBehavior(BindableObject view, bool value)
+        {
+            view.SetValue(AttachBehaviorProperty, value);
+        }
+
+        static void OnAttachBehaviorChanged(BindableObject view, object oldValue, object newValue)
+        {
+            var entry = view as Entry;
+            if (entry == null)
+            {
+                return;
+            }
+
+            bool attachBehavior = (bool)newValue;
+            if (attachBehavior)
+            {
+                entry.TextChanged += OnEntryTextChanged;
+            }
+            else
+            {
+                entry.TextChanged -= OnEntryTextChanged;
+            }
+        }
+
+        static void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            var entry = sender as Entry;
+            if (entry == null)
+            {
+                return;
+            }
+
+            var notifyDataErrorInfo = entry.BindingContext as INotifyDataErrorInfo;
+            if (notifyDataErrorInfo == null)
+            {
+                return;
+            }
+
+            entry.BackgroundColor = notifyDataErrorInfo.HasErrors ? Color.Red : Color.Default;
+        }
+    }
+}
