3981e5d466af6d01b1ca1b722680a5334dc241e5
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-19 10:40:09 +1000

    Replace Android sample app. Pretty close to compiling

---
+++ Adapt.PresentationSamples.Droid/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using System.IO;
+
+using samples = Adapt.PresentationSamples;
+using Adapt.Presentation.AndroidPlatform;
+
+namespace XamForms.Droid
+{
+    [Activity (Label = "XamForms", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+
+            LoadApplication(new samples.App(new PresentationFactory(ApplicationContext), new Permissions()));
+        }
+	}
+}
+
