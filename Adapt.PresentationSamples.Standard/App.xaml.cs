9c5fce18bc219933f4f9626ae4e2b6f99ba8b1b7
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-06-19 11:16:00 +1000

    Android and iOS running

--- Adapt.PresentationSamples.Standard/App.xaml.cs
+++ Adapt.PresentationSamples.Standard/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Adapt.Presentation;
-
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Adapt.PresentationSamples
@@ -13,9 +15,21 @@ namespace Adapt.PresentationSamples
         {
             PresentationFactory = presentationFactory;
             CurrentPermissions = currentPermissions;
+
+            var button = new Button { Text = "Take Photo" };
+            button.Clicked += Button_Clicked;
+            //var mainPage = new MainPage();
+            //mainPage.DisplayAlert("test", "test", "test");
+
+            mainPage = new ContentPage { Title = "test", Content = button };
+
             InitializeComponent();
-            MainPage = new MainPage();
+            MainPage = mainPage;
         }
+
+
+        ContentPage mainPage;
+
 
         protected override void OnStart()
         {
@@ -31,5 +45,41 @@ namespace Adapt.PresentationSamples
         {
             // Handle when your app resumes
         }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            const string defaultFileName = "New Photo.jpg";
+            var media = App.PresentationFactory.CreateMedia(App.CurrentPermissions);
+            var filePicker = App.PresentationFactory.CreateFilePicker();
+
+            var isCameraAvailable = await media.GetIsCameraAvailable();
+
+            if (!isCameraAvailable || !media.IsTakePhotoSupported)
+            {
+                await mainPage.DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            using (var mediaFile = await media.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium,
+                Directory = "Adapt Sample App",
+                Name = defaultFileName
+            }))
+            {
+                using (var readFileStream = mediaFile.GetStream())
+                {
+                    var fileTypes = new Dictionary<string, IList<string>> { { "Jpeg Image", new List<string> { ".jpg" } } };
+
+                    using (var fileData = await filePicker.PickAndOpenFileForWriting(fileTypes, defaultFileName))
+                    {
+                        var readBuffer = new byte[readFileStream.Length];
+                        await readFileStream.ReadAsync(readBuffer, 0, (int)readFileStream.Length);
+                        fileData.FileStream.Write(readBuffer, 0, readBuffer.Length);
+                    }
+                }
+            }
+        }
+
     }
 }
