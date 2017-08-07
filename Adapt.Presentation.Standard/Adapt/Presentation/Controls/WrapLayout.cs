420ba0e8da6da3682acedf2898f05f89f22c4ae8
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-08-07 10:35:00 +1000

    Use regions

--- Adapt.Presentation.Standard/Adapt/Presentation/Controls/WrapLayout.cs
+++ Adapt.Presentation.Standard/Adapt/Presentation/Controls/WrapLayout.cs
@@ -6,13 +6,15 @@ using Xamarin.Forms;
 namespace Adapt.Presentation.Controls
 {
     /// <summary>
-    /// New WrapLayout
+    /// A panel controls that renders its contents from left to right and wraps vertically
     /// </summary>
-    /// <author>Jason Smith</author>
     public class WrapLayout : Layout<View>
     {
+        #region Fields
         private readonly Dictionary<View, SizeRequest> _LayoutCache = new Dictionary<View, SizeRequest>();
+        #endregion
 
+        #region Bindable Properties
         /// <summary>
         /// Backing Storage for the Spacing property
         /// </summary>
@@ -21,7 +23,9 @@ namespace Adapt.Presentation.Controls
             BindableProperty.Create<WrapLayout, double>(w => w.Spacing, 5,
                 propertyChanged: (bindable, oldvalue, newvalue) => ((WrapLayout)bindable)._LayoutCache.Clear());
 #pragma warning restore CS0618 // Type or member is obsolete
+        #endregion
 
+        #region Public Properties
         /// <summary>
         /// Spacing added between elements (both directions)
         /// </summary>
@@ -31,45 +35,18 @@ namespace Adapt.Presentation.Controls
             get => (double)GetValue(SpacingProperty);
             set => SetValue(SpacingProperty, value);
         }
+        #endregion
 
+        #region Constructor
         public WrapLayout()
         {
             BackgroundColor = Color.Red;
-
             VerticalOptions = HorizontalOptions = LayoutOptions.FillAndExpand;
         }
+        #endregion
 
-        protected override void OnChildMeasureInvalidated()
-        {
-            base.OnChildMeasureInvalidated();
-            _LayoutCache.Clear();
-        }
-
-        [Obsolete]
-        protected override SizeRequest OnSizeRequest(double widthConstraint, double heightConstraint)
-        {
-
-            NaiveLayout(widthConstraint, out double lastX, out double lastY);
-
-            return new SizeRequest(new Size(lastX, lastY));
-        }
-
-        protected override void LayoutChildren(double x, double y, double width, double height)
-        {
-            var layout = NaiveLayout(width, out double lastX, out double lastY);
-
-            foreach (var t in layout)
-            {
-                var offset = (int)((width - t.Last().Rectangle.Right) / 2);
-                foreach (var dingus in t)
-                {
-                    var location = new Rectangle(dingus.Rectangle.X + x + offset, dingus.Rectangle.Y + y, dingus.Rectangle.Width, dingus.Rectangle.Height);
-                    LayoutChildIntoBoundingRegion(dingus.View, location);
-                }
-            }
-        }
-
-        private List<ViewAndRectableList> NaiveLayout(double width, out double lastX, out double lastY)
+        #region Private Methods
+        private NaiveLayoutResult NaiveLayout(double width, out double lastX, out double lastY)
         {
             double startX = 0;
             double startY = 0;
@@ -79,7 +56,7 @@ namespace Adapt.Presentation.Controls
             lastX = 0;
             lastY = 0;
 
-            var result = new List<ViewAndRectableList>();
+            var result = new NaiveLayoutResult();
             var currentList = new ViewAndRectableList();
 
             foreach (var child in Children)
@@ -117,7 +94,39 @@ namespace Adapt.Presentation.Controls
             result.Add(currentList);
             return result;
         }
+        #endregion
 
+        #region Protected Overrides
+        protected override void OnChildMeasureInvalidated()
+        {
+            base.OnChildMeasureInvalidated();
+            _LayoutCache.Clear();
+        }
+
+        [Obsolete]
+        protected override SizeRequest OnSizeRequest(double widthConstraint, double heightConstraint)
+        {
+            NaiveLayout(widthConstraint, out double lastX, out double lastY);
+            return new SizeRequest(new Size(lastX, lastY));
+        }
+
+        protected override void LayoutChildren(double x, double y, double width, double height)
+        {
+            var layout = NaiveLayout(width, out double lastX, out double lastY);
+
+            foreach (var t in layout)
+            {
+                var offset = (int)((width - t.Last().Rectangle.Right) / 2);
+                foreach (var dingus in t)
+                {
+                    var location = new Rectangle(dingus.Rectangle.X + x + offset, dingus.Rectangle.Y + y, dingus.Rectangle.Width, dingus.Rectangle.Height);
+                    LayoutChildIntoBoundingRegion(dingus.View, location);
+                }
+            }
+        }
+        #endregion
+
+        #region Private Inner Classes
         private class ViewAndRectangle
         {
             public ViewAndRectangle(View view, Rectangle rectangle)
@@ -135,5 +144,10 @@ namespace Adapt.Presentation.Controls
 
         }
 
+        private class NaiveLayoutResult : List<ViewAndRectableList>
+        {
+
+        }
+        #endregion
     }
 }
