b4b57a48a48ba58063dff5e1ea02e4b9658a0179
Laura <laurab@adaptsolutions.com.au>
2017-09-07 09:11:43 +1000

    Sample working on UWP

--- Adapt.PresentationSamples.Standard/App.xaml.cs
+++ Adapt.PresentationSamples.Standard/App.xaml.cs
@@ -9,7 +9,7 @@ namespace Adapt.PresentationSamples
         public static IPresentationFactory PresentationFactory { get; private set; }
         public static IPermissions CurrentPermissions { get; private set; }
         public static IGeolocator Geolocator { get; private set; }
-        public IClipboard Clipboard { get; set; }
+        public static IClipboard Clipboard { get; set; }
         #endregion
         #region Constructor
         public App(IPresentationFactory presentationFactory, IPermissions currentPermissions, IGeolocator geolocator, IClipboard clipboard)
