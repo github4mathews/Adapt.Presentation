74f9a3cce2f4c21cfb9627b5b2bb1425bd8c987d
Laura <laurab@adaptsolutions.com.au>
2017-09-07 09:24:52 +1000

    Sample working on Android

---
+++ Adapt.Presentation.Android/Adapt/Presentation/AndroidPlatform/Clipboard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Adapt.Presentation;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace apa.Adapt.Presentation.AndroidPlatform
+{
+    public class Clipboard : IClipboard
+    {
+        ClipboardManager ClipboardManager => (ClipboardManager)Application.Context.GetSystemService(Context.ClipboardService);
+
+        public async Task<string> GetClipboardText()
+        {
+            return ClipboardManager.Text;
+        }
+
+        public void SetClipboardText(string data)
+        {
+            ClipboardManager.Text = data;
+        }
+    }
+}
