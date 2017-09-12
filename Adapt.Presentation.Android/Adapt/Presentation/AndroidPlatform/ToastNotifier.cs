6008540895a53472af4cd1e339f9cde50c3b6071
Laura <laurab@adaptsolutions.com.au>
2017-09-12 16:10:11 +1000

    Added Android implementation

---
+++ Adapt.Presentation.Android/Adapt/Presentation/AndroidPlatform/ToastNotifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Adapt.Presentation;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace apa.Adapt.Presentation.AndroidPlatform
+{
+    class ToastNotifier : IInAppNotification
+    {
+        public void Show(string text)
+        {
+            Toast.MakeText(Application.Context, text, ToastLength.Short);
+        }
+    }
+}
