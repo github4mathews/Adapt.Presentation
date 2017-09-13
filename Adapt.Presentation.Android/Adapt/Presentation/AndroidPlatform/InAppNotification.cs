32d68986b04a73aa1e01027bfc4e56619dc84c37
Laura <laurab@adaptsolutions.com.au>
2017-09-13 14:57:59 +1000

    Made name consistant on Android

---
+++ Adapt.Presentation.Android/Adapt/Presentation/AndroidPlatform/InAppNotification.cs
@@ -0,0 +1,14 @@
+﻿using Adapt.Presentation;
+using Android.Widget;
+using Application = Android.App.Application;
+
+namespace apa.Adapt.Presentation.AndroidPlatform
+{
+    public class InAppNotification : IInAppNotification
+    {
+        public void Show(string text)
+        {
+            Toast.MakeText(Application.Context, text, ToastLength.Short).Show();
+        }
+    }
+}
