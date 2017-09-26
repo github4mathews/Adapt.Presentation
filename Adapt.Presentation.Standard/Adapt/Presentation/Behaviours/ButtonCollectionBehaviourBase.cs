05948f89a57623b954a408f65489062f33353e3a
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-09-27 09:31:56 +1000

    Add some button behaviours for use with AdaptListView etc.

---
+++ Adapt.Presentation.Standard/Adapt/Presentation/Behaviours/ButtonCollectionBehaviourBase.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace Adapt.Presentation.Behaviours
+{
+    public class ButtonCollectionBehaviourBase : Behavior<Button>
+    {
+        #region Fields
+        protected Button Button;
+        #endregion
+
+        #region BindableProperty
+
+        #region Collection
+        public static readonly BindableProperty CollectionProperty =
+        BindableProperty.CreateAttached(
+        "Collection",
+        typeof(IList),
+        typeof(Button),
+        null,
+        propertyChanged: OnCollectionChanged);
+
+        private static void OnCollectionChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            SetCollection(bindable, (IList)newValue);
+        }
+
+        public static IList GetCollection(BindableObject view)
+        {
+            return (IList)view.GetValue(CollectionProperty);
+        }
+
+        public static void SetCollection(BindableObject view, IList value)
+        {
+            var button = (Button)view;
+            view.SetValue(CollectionProperty, value);
+            if (value is INotifyCollectionChanged notifyCollectionChanged)
+            {
+                RaiseCollectionChanged(button);
+            }
+
+            RaiseCollectionChanged(button);
+        }
+        #endregion
+
+        #endregion
+
+        #region Protected Overrides
+        protected override void OnAttachedTo(Button bindable)
+        {
+            base.OnAttachedTo(bindable);
+            Button = bindable;
+            bindable.Clicked += Button_Clicked;
+        }
+
+        protected override void OnDetachingFrom(Button bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.Clicked -= Button_Clicked;
+        }
+        #endregion
+
+        #region Virtual Methods
+
+        protected virtual void OnCollectionChanged(Button button) { }
+
+        protected virtual void OnButtonClicked(Button button)
+        {
+        }
+        #endregion
+
+        #region Event Handlers
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            OnButtonClicked(sender as Button);
+        }
+        #endregion
+
+        #region Private Static Methods
+        private static void RaiseCollectionChanged(Button button)
+        {
+            //TODO: Why is this necessary in XF? Is there a better way?
+            var behaviour = (ButtonCollectionBehaviourBase)button.Behaviors.Where(b => (b is ButtonCollectionBehaviourBase)).FirstOrDefault();
+            if (behaviour == null)
+            {
+                //This shouldn't be possible...
+                return;
+            }
+
+            behaviour.OnCollectionChanged(button);
+        }
+        #endregion
+    }
+}
---
+++ Adapt.Presentation.Standard/Adapt/Presentation/Behaviours/RemoveFromCollectionBehavior.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Specialized;
+using Xamarin.Forms;
+
+namespace Adapt.Presentation.Behaviours
+{
+    public class RemoveFromCollectionBehavior : ButtonCollectionBehaviourBase
+    {
+        #region BindableProperty
+        #region RemoveItems
+        public static readonly BindableProperty RemoveItemsProperty =
+        BindableProperty.CreateAttached(
+        "RemoveItems",
+        typeof(IList),
+        typeof(Button),
+        null,
+        propertyChanged: OnRemoveItemsChanged);
+
+        private static void OnRemoveItemsChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            SetRemoveItems(bindable, (IList)newValue);
+        }
+
+        public static IList GetRemoveItems(BindableObject view)
+        {
+            return (IList)view.GetValue(RemoveItemsProperty);
+        }
+
+        public static void SetRemoveItems(BindableObject view, IList value)
+        {
+            view.SetValue(RemoveItemsProperty, value);
+            var button = (Button)view;
+            if (value is INotifyCollectionChanged notifyCollectionChanged)
+            {
+                notifyCollectionChanged.CollectionChanged += (s, e) => { IsEnabled(button, value); };
+            }
+            IsEnabled(button, value);
+        }
+        #endregion
+        #endregion
+
+        #region Protected Overrides
+        protected override void OnAttachedTo(Button bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.IsEnabled = false;
+        }
+
+        protected override void OnButtonClicked(Button button)
+        {
+            base.OnButtonClicked(button);
+
+            var collection = (IList)Button.GetValue(CollectionProperty);
+            if (collection == null)
+            {
+                return;
+            }
+
+            var removeItems = (IList)Button.GetValue(RemoveItemsProperty);
+            if (removeItems == null)
+            {
+                return;
+            }
+
+            //Get the binding context
+            var bindingContext = button.BindingContext;
+
+            if (removeItems != null)
+            {
+                for (var i = removeItems.Count - 1; i > -1; i--)
+                {
+                    collection.Remove(removeItems[i]);
+                }
+            }
+            else if (bindingContext == null)
+            {
+                collection.Remove(bindingContext);
+            }
+        }
+
+        #endregion
+
+        #region Private Static Methods
+        private static void IsEnabled(Button button, IList list)
+        {
+            button.IsEnabled = list.Count > 0;
+        }
+        #endregion
+    }
+}
