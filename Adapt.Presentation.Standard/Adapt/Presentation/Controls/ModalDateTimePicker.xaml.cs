f29d5f66f8d93e1bffc03fa7ac5c39e575af3b1e
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-09-26 14:55:29 +1000

    Add the ModalDateTimePicker
    Refactoring

---
+++ Adapt.Presentation.Standard/Adapt/Presentation/Controls/ModalDateTimePicker.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms.Xaml;
+
+namespace Adapt.Presentation.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ModalDateTimePicker
+    {
+        #region Events
+        public event EventHandler Closing;
+        #endregion
+
+        #region Fields
+        private bool _IsOkClicked;
+        #endregion
+
+        #region Public Properties
+        public DateTime SelectedValue
+        {
+            get
+            {
+                DateTime date;
+
+                date = TheCalendar.Date.Date == new DateTime(1900, 1, 1) ? DateTime.MinValue : TheCalendar.Date.Date;
+
+                return date + TheTimePicker.Time;
+            }
+            set
+            {
+                TheCalendar.Date = value == DateTime.MinValue ? DateTime.Now.Date : value.Date;
+
+                TheTimePicker.Time = value - value.Date;
+            }
+        }
+
+        public bool IsOkClicked => _IsOkClicked;
+
+        #endregion
+
+        #region Constructor
+        public ModalDateTimePicker()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Event Handlers
+        private async void OKButton_Clicked(object sender, EventArgs e)
+        {
+            _IsOkClicked = true;
+            await Close();
+        }
+
+        private async void CancelButton_Clicked(object sender, EventArgs e)
+        {
+            _IsOkClicked = false;
+            await Close();
+        }
+
+        private async Task Close()
+        {
+            Closing?.Invoke(this, new EventArgs());
+            await Navigation.PopModalAsync();
+        }
+
+        private void Clear_Clicked(object sender, EventArgs e)
+        {
+            TheCalendar.Date = DateTime.MinValue;
+            TheTimePicker.Time = new TimeSpan();
+        }
+        private void Now_Clicked(object sender, EventArgs e)
+        {
+            TheCalendar.Date = DateTime.Now;
+            TheTimePicker.Time = DateTime.Now.TimeOfDay;
+        }
+        #endregion
+
+    }
+}
---
+++ Adapt.Presentation.Standard/Adapt/Presentation/Controls/ModalDateTimePickerButton.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace Adapt.Presentation.Controls
+{
+    public class ModalDateTimePickerButton : Button
+    {
+        #region Fields
+        private readonly ModalDateTimePicker _ModalDateTimePicker;
+        #endregion
+
+        #region Public Properties
+        public string DateTimePropertyName { get; set; }
+        #endregion
+
+        #region Constructor
+        public ModalDateTimePickerButton()
+        {
+            _ModalDateTimePicker = new ModalDateTimePicker();
+            Clicked += ModalDateTimePickerButton_Clicked;
+            _ModalDateTimePicker.Closing += ModalDateTimePicker_Closing;
+        }
+        #endregion
+
+        #region Event Handlers
+        private async void ModalDateTimePickerButton_Clicked(object sender, EventArgs e)
+        {
+            var dateTimeProperty = GetDateTimeProperty();
+            var value = (DateTime)dateTimeProperty.GetValue(BindingContext);
+            _ModalDateTimePicker.SelectedValue = value;
+            await Navigation.PushModalAsync(_ModalDateTimePicker);
+        }
+
+        private void ModalDateTimePicker_Closing(object sender, EventArgs e)
+        {
+            if (!_ModalDateTimePicker.IsOkClicked)
+            {
+                return;
+            }
+
+            var dateTimeProperty = GetDateTimeProperty();
+            var selectValue = _ModalDateTimePicker.SelectedValue;
+            dateTimeProperty.SetValue(BindingContext, selectValue);
+        }
+        #endregion
+
+        #region Private Methods
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "DateTimePropertyName")]
+        private PropertyInfo GetDateTimeProperty()
+        {
+            if (BindingContext == null)
+            {
+                throw new Exception("There is no BindingContext");
+            }
+
+            if (string.IsNullOrEmpty(DateTimePropertyName))
+            {
+                throw new Exception($"{nameof(DateTimePropertyName)} must be specified.");
+            }
+
+            var bindingContextType = BindingContext.GetType();
+
+            var dateTimeProperty = bindingContextType.GetRuntimeProperty(DateTimePropertyName);
+            if (dateTimeProperty == null)
+            {
+                throw new Exception($"{DateTimePropertyName} does not exist on the type {bindingContextType.FullName}");
+            }
+
+            return dateTimeProperty;
+        }
+        #endregion
+    }
+}
