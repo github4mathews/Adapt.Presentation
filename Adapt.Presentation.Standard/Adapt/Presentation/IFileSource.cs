64d9eb647e3b39288f9bf57f17a746ca2edfbccb
Laura <laurab@adaptsolutions.com.au>
2017-10-06 10:46:57 +1100

    Added standard interface

---
+++ Adapt.Presentation.Standard/Adapt/Presentation/IFileSource.cs
@@ -0,0 +1,15 @@
+﻿namespace Adapt.Presentation
+{
+    public interface IFileSource
+    {
+        string GetFileString(string name, IFileSourceType type);
+    }
+
+
+    /// <summary>
+    /// Used in implementations to switch between folders or catalogs when getting a file string
+    /// </summary>
+    public interface IFileSourceType
+    {
+    }
+}
