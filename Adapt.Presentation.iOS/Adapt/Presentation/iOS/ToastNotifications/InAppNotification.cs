84db73f2bb400913af2cc33390450cde965e3fa0
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-10-06 09:22:24 +1100

    Delete unused properties etc.

--- Adapt.Presentation.iOS/Adapt/Presentation/iOS/ToastNotifications/InAppNotification.cs
+++ Adapt.Presentation.iOS/Adapt/Presentation/iOS/ToastNotifications/InAppNotification.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using UIKit;
@@ -9,9 +8,12 @@ namespace Adapt.Presentation.iOS.ToastNotifications
 {
     public class InAppNotification : IInAppNotification
     {
+        #region Private Fields
         private readonly UnNotificationManager _NotificationManager;
         private readonly LocalNotificationManager _LocalNotificationManager;
+        #endregion
 
+        #region Constructor
         public InAppNotification()
         {
             if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
@@ -23,10 +25,10 @@ namespace Adapt.Presentation.iOS.ToastNotifications
                 _LocalNotificationManager = new LocalNotificationManager();
             }
         }
+        #endregion
 
-        public static void Init() { }
-
-        public async Task<INotificationResult> Notify(INotificationOptions options)
+        #region Private Methods
+        private async Task<INotificationResult> Notify(INotificationOptions options)
         {
             INotificationResult result = null;
             return await Task.Run(() =>
@@ -46,39 +48,12 @@ namespace Adapt.Presentation.iOS.ToastNotifications
             });
         }
 
-        public void Notify(Action<INotificationResult> callback, INotificationOptions options)
+        private void Notify(Action<INotificationResult> callback, INotificationOptions options)
         {
             Task.Run(async () => callback(await Notify(options)));
         }
 
-        public async Task<IList<INotification>> GetDeliveredNotifications()
-        {
-            IList<INotification> list = new List<INotification>();
-
-            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
-            {
-                var notificationCenter = UNUserNotificationCenter.Current;
-
-                var deliveredNotifications = await notificationCenter.GetDeliveredNotificationsAsync();
-
-                foreach (var notification in deliveredNotifications)
-                {
-
-                    UNNotificationContent content = notification.Request.Content;
-                    list.Add(new Notification()
-                    {
-                        Id = notification.Request.Identifier,
-                        Title = content.Title,
-                        Description = content.Body,
-                        Delivered = notification.Date.ToDateTime()
-                    });
-                }
-            }
-
-            return list;
-        }
-
-        public void CancelAllDelivered()
+        private void CancelAllDelivered()
         {
             if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
             {
@@ -87,10 +62,13 @@ namespace Adapt.Presentation.iOS.ToastNotifications
                 notificationCenter.RemoveAllDeliveredNotifications();
             }
         }
+        #endregion
 
+        #region Public Methods
         public async void Show(string text)
         {
             await Notify(new NotificationOptions() { Title = text, Description = text });
         }
+        #endregion
     }
 }
