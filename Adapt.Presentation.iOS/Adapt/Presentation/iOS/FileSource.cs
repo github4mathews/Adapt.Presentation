ba19efa83c69ee93f277cf4f1bfa967bbb7f2a9b
Laura <laurab@adaptsolutions.com.au>
2017-10-06 13:15:51 +1100

    Fixed iOS image referencing

--- Adapt.Presentation.iOS/Adapt/Presentation/iOS/FileSource.cs
+++ Adapt.Presentation.iOS/Adapt/Presentation/iOS/FileSource.cs
@@ -7,7 +7,7 @@ namespace Adapt.Presentation.iOS
         public string GetFileString(string name, FileSourceType type)
         {
             //I don't actually know if this is right, it will probably be wrong though, iOS does like, catalogs and stuff and I don't understand how it works
-            if (type == FileSourceType.Image) return Path.ChangeExtension(name.ToLower(), "png");
+            if (type == FileSourceType.Image) return Path.GetFileNameWithoutExtension(name);
             return name.ToLower();
         }
     }
