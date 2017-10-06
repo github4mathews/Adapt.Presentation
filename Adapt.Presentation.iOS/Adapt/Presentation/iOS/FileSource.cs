f818f55106824ec8dd5d1c094992ad71c1dbad9a
Laura <laurab@adaptsolutions.com.au>
2017-10-06 12:59:58 +1100

    Implemented in iOS
    TODO: Test

--- Adapt.Presentation.iOS/Adapt/Presentation/iOS/FileSource.cs
+++ Adapt.Presentation.iOS/Adapt/Presentation/iOS/FileSource.cs
@@ -1,11 +1,13 @@
+using System.IO;
+
 namespace Adapt.Presentation.iOS
 {
     public class FileSource : IFileSource
     {
-        public string GetFileString(string name, IFileSourceType type)
+        public string GetFileString(string name, FileSourceType type)
         {
-            //TODO: Implement type
-            //I don't actually know if this is right, it will probably be wrong though, iOS does like, catalogs and stuff
+            //I don't actually know if this is right, it will probably be wrong though, iOS does like, catalogs and stuff and I don't understand how it works
+            if (type == FileSourceType.Image) return Path.ChangeExtension(name.ToLower(), "png");
             return name.ToLower();
         }
     }
