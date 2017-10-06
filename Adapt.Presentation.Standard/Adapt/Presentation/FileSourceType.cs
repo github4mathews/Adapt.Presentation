320b2b21359e25ce9867b292adfcafdc0511e178
MelbourneDeveloper <christian_findlay@hotmail.com>
2017-10-06 13:31:55 +1100

    Split a file out

---
+++ Adapt.Presentation.Standard/Adapt/Presentation/FileSourceType.cs
@@ -0,0 +1,10 @@
+﻿namespace Adapt.Presentation
+{
+    /// <summary>
+    /// Used in implementations to switch between folders or catalogs when getting a file string
+    /// </summary>
+    public enum FileSourceType
+    {
+        Image
+    }
+}
--- Adapt.Presentation.Standard/Adapt/Presentation/IFileSource.cs
+++ Adapt.Presentation.Standard/Adapt/Presentation/IFileSource.cs
@@ -4,13 +4,4 @@
     {
         string GetFileString(string name, FileSourceType type);
     }
-
-
-    /// <summary>
-    /// Used in implementations to switch between folders or catalogs when getting a file string
-    /// </summary>
-    public enum FileSourceType
-    {
-        Image
-    }
 }
