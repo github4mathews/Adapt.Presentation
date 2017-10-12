b2a91c85c2de5051405f6e6903043a42624f461a
Laura <laurab@adaptsolutions.com.au>
2017-10-13 10:29:19 +1100

    Got the renderer to load, albeit yuckily

--- Adapt.Presentation.UWP/Adapt/Presentation/UWP/ContextMenuFactory.cs
+++ Adapt.Presentation.UWP/Adapt/Presentation/UWP/ContextMenuFactory.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Adapt.Presentation.UWP.Adapt.Presentation.UWP;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.UWP;
 
+//TODO: Possible to dependency inject this manually?
 [assembly: Dependency(typeof(ContextMenuFactory))]
 namespace Adapt.Presentation.UWP.Adapt.Presentation.UWP
 {
@@ -17,6 +22,7 @@ namespace Adapt.Presentation.UWP.Adapt.Presentation.UWP
 
 		public void Attach(View bindable, IList<MenuItem> contextActions)
 		{
+			//Build the flyout
 			var flyout = new MenuFlyout();
 			foreach (var action in contextActions)
 			{
@@ -24,8 +30,25 @@ namespace Adapt.Presentation.UWP.Adapt.Presentation.UWP
 				flyout.Items.Add(new MenuFlyoutItem { Command = action.Command, Text = action.Text });
 			}
 
-			var renderer = Platform.GetRenderer(bindable);
-			FlyoutBase.SetAttachedFlyout(renderer.ContainerElement, flyout);
+			//This is fucking nasty
+			var mainThread = SynchronizationContext.Current;
+			new TaskFactory().StartNew(() =>
+			{
+				//Wait for the renderer e.e
+				IVisualElementRenderer renderer = null;
+				while (renderer == null)
+				{
+					renderer = Platform.GetRenderer(bindable);
+				}
+
+				//Renderer is all good, set the flyout and the event
+				mainThread.Post(arg =>
+				{
+					var nativeElement = renderer.ContainerElement;
+					FlyoutBase.SetAttachedFlyout(nativeElement, flyout);
+					nativeElement.RightTapped += (s, e) => FlyoutBase.ShowAttachedFlyout(nativeElement);
+				}, null);
+			});
 		}
 	}
 }
