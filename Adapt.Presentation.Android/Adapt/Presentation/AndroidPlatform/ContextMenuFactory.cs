df1639a28888fc451f34b07460eb03cf8af6c9be
Laura <laurab@adaptsolutions.com.au>
2017-10-13 13:22:57 +1100

    Logic done for Android

---
+++ Adapt.Presentation.Android/Adapt/Presentation/AndroidPlatform/ContextMenuFactory.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using apa.Adapt.Presentation.AndroidPlatform;
+using Adapt.Presentation;
+using Adapt.Presentation.Behaviours;
+using Android.App;
+using Android.Runtime;
+using Android.Support.V7.View;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using View = Xamarin.Forms.View;
+
+//TODO: Possible to dependency inject this manually?
+[assembly: Dependency(typeof(ContextMenuFactory))]
+
+namespace apa.Adapt.Presentation.AndroidPlatform
+{
+    class ContextMenuFactory : IContextMenuFactory
+    {
+        public void Detach(View bindable)
+        {
+            ContextMenu.Dispose();
+        }
+
+        public void Attach(View bindable, IList<ContextMenuItem> contextActions)
+        {
+            //Set properties
+            ContextActions = contextActions;
+            View = bindable;
+
+            //TODO: Create long tap recogniser
+            View.GestureRecognizers.Add(new TapGestureRecognizer(ObjectLongTapped));
+
+            //Set the renderer, it won't be created yet :/
+            var renderer = Platform.CreateRenderer(View);
+            Platform.SetRenderer(View, renderer);
+
+            //Create a context menu and add the items
+            ContextMenu = new PopupMenu(Forms.Context, (Android.Views.View)renderer);
+            foreach (var action in ContextActions)
+            {
+                ContextMenu.Menu.Add(Menu.None, ContextActions.IndexOf(action), Menu.None, action.Text);
+            }
+
+            //Handle item clicks
+            ContextMenu.MenuItemClick += ContextMenu_MenuItemClick;
+        }
+
+        public IList<ContextMenuItem> ContextActions { get; set; }
+        public PopupMenu ContextMenu { get; set; }
+        public View View { get; private set; }
+
+        private void ObjectLongTapped(View obj)
+        {
+            ContextMenu.Show();
+        }
+
+        private void ContextMenu_MenuItemClick(object sender, PopupMenu.MenuItemClickEventArgs e)
+        {
+            ContextActions[e.Item.ItemId].InvokeClicked();
+        }
+    }
+}
--- Adapt.Presentation.Standard/Adapt/Presentation/Behaviours/ContextMenuBehavior.cs
+++ Adapt.Presentation.Standard/Adapt/Presentation/Behaviours/ContextMenuBehavior.cs
@@ -3,21 +3,22 @@ using Xamarin.Forms;
 
 namespace Adapt.Presentation.Behaviours
 {
-	public class ContextMenuBehavior : Behavior<View>
-	{
-		public IList<ContextMenuItem> ContextActions => (IList<ContextMenuItem>)GetValue(ContextActionsProperty);
-		public static BindableProperty ContextActionsProperty = BindableProperty.Create(nameof(ContextActions), typeof(IList<ContextMenuItem>), typeof(ContextMenuBehavior), new List<ContextMenuItem>());
+    public class ContextMenuBehavior : Behavior<View>
+    {
+        public IList<ContextMenuItem> ContextActions => (IList<ContextMenuItem>)GetValue(ContextActionsProperty);
+        public static BindableProperty ContextActionsProperty = BindableProperty.Create(nameof(ContextActions), typeof(IList<ContextMenuItem>), typeof(ContextMenuBehavior), new List<ContextMenuItem>());
+        readonly IContextMenuFactory _ContextMenu = DependencyService.Get<IContextMenuFactory>(DependencyFetchTarget.NewInstance);
 
-		protected override void OnAttachedTo(View bindable)
-		{
-			base.OnAttachedTo(bindable);
-			DependencyService.Get<IContextMenuFactory>().Attach(bindable, ContextActions);
-		}
+        protected override void OnAttachedTo(View bindable)
+        {
+            base.OnAttachedTo(bindable);
+            _ContextMenu.Attach(bindable, ContextActions);
+        }
 
-		protected override void OnDetachingFrom(View bindable)
-		{
-			base.OnDetachingFrom(bindable);
-			DependencyService.Get<IContextMenuFactory>().Detach(bindable);
-		}
-	}
+        protected override void OnDetachingFrom(View bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            _ContextMenu.Detach(bindable);
+        }
+    }
 }
