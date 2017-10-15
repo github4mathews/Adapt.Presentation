69c6d80e5c844b58d5e12b4a48cc4106675b3c62
Laura <laurab@adaptsolutions.com.au>
2017-10-16 10:57:56 +1100

    Added long tap recognizer to iOS

--- Adapt.Presentation.iOS/Adapt/Presentation/iOS/ContextMenuFactory.cs
+++ Adapt.Presentation.iOS/Adapt/Presentation/iOS/ContextMenuFactory.cs
@@ -4,6 +4,7 @@ using Adapt.Presentation.iOS;
 using UIKit;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
+using Platform = Xamarin.Forms.Platform.iOS.Platform;
 using View = Xamarin.Forms.View;
 
 //TODO: Possible to dependency inject this manually?
@@ -13,6 +14,8 @@ namespace Adapt.Presentation.iOS
 {
     class ContextMenuFactory : IContextMenuFactory
     {
+        private IVisualElementRenderer _TheRenderer;
+
         public void Detach(View bindable)
         {
             ContextMenu.Dispose();
@@ -21,8 +24,8 @@ namespace Adapt.Presentation.iOS
         public void Attach(View bindable, IList<ContextMenuItem> contextActions)
         {
             //Set the renderer, it won't be created yet :/
-            var renderer = Platform.CreateRenderer(bindable);
-            Platform.SetRenderer(bindable, renderer);
+            _TheRenderer = Platform.CreateRenderer(bindable);
+            Platform.SetRenderer(bindable, _TheRenderer);
 
             //Create a context menu and add the items
             ContextMenu = UIAlertController.Create(null, null, UIAlertControllerStyle.ActionSheet);
@@ -31,15 +34,21 @@ namespace Adapt.Presentation.iOS
                 ContextMenu.AddAction(UIAlertAction.Create(action.Text, UIAlertActionStyle.Default, a => action.InvokeClicked()));
             }
 
-            //Set location to popout from
-            ContextMenu.PopoverPresentationController.SourceView = renderer.NativeView;
-            ContextMenu.PopoverPresentationController.SourceRect = renderer.NativeView.Frame;
+            //Attach the long tap recogniser
+            var longTapRecogniser = new UILongPressGestureRecognizer();
+            longTapRecogniser.AddTarget(ShowContextMenu);
+            _TheRenderer.NativeView.AddGestureRecognizer(longTapRecogniser);
         }
 
         public UIAlertController ContextMenu { get; set; }
 
         public void ShowContextMenu()
         {
+            //Set location to popout from
+            ContextMenu.PopoverPresentationController.SourceView = _TheRenderer.NativeView;
+            ContextMenu.PopoverPresentationController.SourceRect = _TheRenderer.NativeView.Frame;
+
+            //Present the context menu
             var rootViewController = UIApplication.SharedApplication.KeyWindow.RootViewController;
             rootViewController.PresentViewController(ContextMenu, true, null);
         }
